b609ddef775623796596defccb57bd88250fecc9
michiru6828 <since_l999@yahoo.co.jp>
2023-06-06 16:53:24 -0700

    added delete and edit button

--- frontend/src/component/ShowList.tsx
+++ frontend/src/component/ShowList.tsx
@@ -1,10 +1,15 @@
 import React from "react";
+import Button from "./Button";
 
 function ShowList({ list }: { list: string[] }) {
   return (
     <div>
       {list.map((item: string) => (
-        <li key={item}>{item}</li>
+        <li key={item}>
+          {item}
+          <Button text={"削除"} onClick={() => console.log("deleted")} />
+          <Button text={"編集"} onClick={() => console.log("edited")} />
+        </li>
       ))}
     </div>
   );
